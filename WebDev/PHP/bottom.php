34ad4bc412b13503c15698c5b955f02219253243
Samridhi Kohli <42908015+samridhikohli@users.noreply.github.com>
2019-03-14 16:53:10 +0530

    Add files via upload

---
+++ WebDev/PHP/bottom.php
@@ -0,0 +1,65 @@
+<div class="fotter">	
+	 <div class="container">
+		 <div class="fotter-grids">
+			 <div class="col-md-4 fotter-left">
+			 <img src="images/fotter-logo.png" alt="">
+			 <p style="text-align:justify">This is an informative website made by Samridhi,Pulkit and Rashee.
+			 We tend to give information about the various tourist places in the World. 
+                         We share this blog so you can find useful information for your next tour. </p>
+			 </div>
+			 <div class="col-md-4 fotter-middle">
+				 <h3>Questions About Our Tours?</h3>
+				 <div class="footer-list">
+						<ul>
+                        <li style="text-align:justify"><a href="#"><span></span>
+                        Our Customer Service Department is always happy to answer any questions you might have. Please visit our Contact Page to contact us or call us at 9660407467.</a></li></ul> </div>
+						<h3>why makemytrip?</h3>
+                        <div class="footer-list">
+                        <ul>
+
+ <li style="text-align:justify"><a href="#"><span></span>Customer Satisfaction<br/></a></li>
+ <li style="text-align:justify"><a href="#"><span></span>Superior customer service, 24x7 Dedicated helpline and over 5 million delighted customers & still growing<br/></a></li>
+<li style="text-align:justify"><a href="#"><span></span>Best Deals Guaranteed<br/></a></li>
+<li style="text-align:justify"><a href="#"><span></span>Great experiences at lowest prices guaranteed.<br/></a></li>
+</a></li> </ul>
+			 </div>
+             
+			 </div>
+			 <div class="col-md-4 fotter-right" style="padding-left:70px">
+			 <h3>Why buy from us?</h3><br/>
+              <div class="footer-list">
+			  <ul>
+              <li style="color:#FFF;text-align:justify"><a href="#" style="color:#FFF"><span></span>Travel Guide</br></a></li>
+	      <li style="color:#FFF;text-align:justify"><a href="#"style="color:#FFF"><span></span>Payment Security</br></a></li>
+              <li style="color:#FFF;text-align:justify"><a href="#"style="color:#FFF"><span></span>Social Responsibility</br></a></li>
+               </ul> </div></div>
+			 <div class="social-icons">
+			 <a href="#"><span class="facebook"> </span></a>
+			 <a href="#"><span class="twitter"> </span></a>
+			 <a href="#"><span class="googleplus"> </span></a>
+			 <a href="#"><span class="pinterest"> </span></a>
+			 <a href="#"><span class="instagram"> </span></a>
+			 </div>
+			 <div class="clearfix"></div>
+	     </div>
+		 <div class="clearfix"></div>
+	 </div>
+</div>
+</div>  
+<div class="copyright text-right">
+</div>
+<script src="js/jquery.scrollTo.js"></script>
+	<script src="js/jquery.nav.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+		$('#nav').onePageNav({
+			begin: function() {
+			console.log('start')
+			},
+			end: function() {
+			console.log('stop')
+			}
+		});
+	});
+	</script>
+  
